88dc3dfb5a3119e6e35c89b3f539759c01c84f21
Guilherme Possebon <gpossebon67@gmail.com>
2022-12-26 08:43:38 -0300

    Modificando e aprendendo

--- Desafios/Desafio001/NotasEscolares.js
+++ Desafios/Desafio001/NotasEscolares.js
@@ -4,21 +4,37 @@
 // D = 60 - 69
 // F = 0 - 59
 
-let grade = 99
-let gradeA = grade >= 90
-let gradeB = grade >= 80 && grade < 90
-let gradeC = grade >= 70 && grade < 80
-let gradeD = grade >= 60 && grade < 70
-let gradeF = grade < 60
+function getGrade(grade) {
+    let gradeA = grade >= 90 && grade <= 100
+    let gradeB = grade >= 80 && grade <= 89
+    let gradeC = grade >= 70 && grade <= 79
+    let gradeD = grade >= 60 && grade <= 69
+    let gradeF = grade < 60 && grade >= 0
 
-if (gradeA) {
-    console.log('Notas altas');
-} else if (gradeB) {
-    console.log('Notas boas');
-} else if (gradeC) {
-    console.log('Notas acima da média');
-} else if (gradeD) {
-    console.log('Notas na média');
-} else if (gradeF) {
-    console.log('Notas abaixo da média');
+    let finalGrade = ''
+
+    if (gradeA) {
+        console.log('Nota A');
+    } else if (gradeB) {
+        console.log('Nota B');
+    } else if (gradeC) {
+        console.log('Nota C');
+    } else if (gradeD) {
+        console.log('Nota D');
+    } else if (gradeF) {
+        console.log('Nota F');
+    } else {
+        console.log('Nota invalida');
+    }
+
+    return finalGrade
 }
+
+console.log( getGrade(2345));
+console.log( getGrade(24));
+console.log( getGrade(-1));
+console.log( getGrade(67));
+console.log( getGrade(95));
+console.log( getGrade(101));
+console.log( getGrade(80));
+console.log( getGrade(72));
