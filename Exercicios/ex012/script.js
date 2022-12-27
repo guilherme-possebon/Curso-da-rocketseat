37ebe1247ac5d0dbb5e16ba406275c60ce69902c
Guilherme Possebon <gpossebon67@gmail.com>
2022-12-27 20:08:33 -0300

    Praticando o uso da DOM

---
+++ Exercicios/ex012/script.js
@@ -0,0 +1,20 @@
+// const element = document.getElementById('blog-title')
+// console.log(element);
+
+// const element = document.getElementsByClassName('one')
+// console.log(element);
+
+// const element = document.getElementsByTagName('meta')
+// console.log(element);
+
+// const element = document.querySelector('body')
+// console.log(element);
+
+// const element = document.querySelectorAll('[src]')
+// console.log(element);
+
+// getElementById (element)
+// getElementsByClassName (HTMLCollection)
+// getElementsByTagName (HTMLCollection)
+// querySelector (element)
+// querySelectorAll (NodeList)
---
+++ Exercicios/ex012/textContent.js
@@ -0,0 +1,24 @@
+// const element = document.querySelector('header')
+
+// element.textContent += ", ola"
+// element.innerText = 'ola'
+// element.innerHTML = 'Ola <img src="pattern001.png" alt="">'
+// element.value = 'aaaaaaaaaaaaa'
+// element.setAttribute('Id', 'cabecalho')
+
+// const headerId = document.querySelector('#cabecalho')
+// console.log(headerId.getAttribute('id'));
+
+// header.removeAttribute('id')
+// header.removeAttribute('class')
+// element.setAttribute('class', 'classe classe2')
+
+// const element = document.querySelector('body')
+// element.style.backgroundColor = 'blue'
+
+const element = document.querySelector('h1')
+
+element.classList.add('text')
+// element.classList.remove('text')
+element.classList.toggle('text')
+// O toggle é como se fosse um interruptor, se está ligado, ele desliga, se estiver desligado, ele liga
